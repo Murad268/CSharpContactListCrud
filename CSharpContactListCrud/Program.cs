cffaa463a32274e473fe16c809d191e97e702859
Murad <agamedov94@mail.ru>
2024-07-07 15:25:59 +0400

    commit

---
+++ CSharpContactListCrud/Program.cs
@@ -0,0 +1,130 @@
+﻿using CSharpContactListCrud.controllers;
+using System;
+using System.Text;
+
+namespace CSharpContactListCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            ContactListController controller = new ContactListController();
+            Boolean end = true;
+            while (end)
+            {
+                Console.WriteLine("Kontakt siyahısına baxmaq üçün 1, Kontak əlavə etmək üçün 2, Kontaktı silmək üçün 3, Kontaktı axtarmaq üçün 4, Kontaktı yeniləmək üçün 5, Çıxmaq üçün 0 seçin");
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            controller.viewContacts();
+                            break;
+                        case 2:
+                            Console.WriteLine("Kontaktın adını daxil edin");
+                            string fullName = Console.ReadLine();
+                            Console.WriteLine("Kontaktın nömrəsini daxil edin");
+                            string number = Console.ReadLine();
+                            controller.addContact(fullName, number);
+                            break;
+                        case 3:
+                            controller.viewContacts();
+                            Console.WriteLine("Silmək istədiyiniz kontaktın id-sini daxil edin");
+                            int id;
+                            while (true)
+                            {
+                                string idInput = Console.ReadLine();
+                                if (idInput == "geri")
+                                {
+                                    break;
+                                }
+                                if (int.TryParse(idInput, out id))
+                                {
+                                    controller.removeContact(id);
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Düzgün ID daxil edin və ya geri yazın:");
+                                }
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Axtarış meyarını seçin: Ad üçün 1, Nömrə üçün 2, ID üçün 3");
+                            int searchType;
+                            if (int.TryParse(Console.ReadLine(), out searchType))
+                            {
+                                Console.WriteLine("Axtarış datasını daxil edin");
+                                string query = Console.ReadLine();
+                                controller.SearchContact(query, searchType);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Düzgün seçim daxil edin");
+                            }
+                            break;
+                        case 5:
+                            controller.viewContacts();
+                            int updateId;
+                            while (true)
+                            {
+                                Console.WriteLine("Yeniləmək istədiyiniz kontaktın id-sini daxil edin və ya geri yazın:");
+                                string idInput = Console.ReadLine();
+                                if (idInput == "geri")
+                                {
+                                    break;
+                                }
+                                if (int.TryParse(idInput, out updateId))
+                                {
+                                    Console.WriteLine("Yeniləmək istədiyiniz məlumatı seçin: Ad üçün 1, Nömrə üçün 2");
+                                    int updateChoice;
+                                    if (int.TryParse(Console.ReadLine(), out updateChoice))
+                                    {
+                                        switch (updateChoice)
+                                        {
+                                            case 1:
+                                                Console.WriteLine("Yeni adı daxil edin");
+                                                string newFullName = Console.ReadLine();
+                                                controller.updateContactName(updateId, newFullName);
+                                                break;
+                                            case 2:
+                                                Console.WriteLine("Yeni nömrəni daxil edin");
+                                                string newNumber = Console.ReadLine();
+                                                controller.updateContactNumber(updateId, newNumber);
+                                                break;
+                                            default:
+                                                Console.WriteLine("Düzgün seçim daxil edin");
+                                                break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Düzgün seçim daxil edin");
+                                    }
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Düzgün ID daxil edin və ya geri yazın:");
+                                }
+                            }
+                            break;
+                        case 0:
+                            end = false;
+                            break;
+                        default:
+                            Console.WriteLine("Düzgün seçim daxil edin");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Düzgün seçim daxil edin");
+                }
+            }
+        }
+    }
+}
---
+++ CSharpContactListCrud/controllers/ContactListController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpContactListCrud.models;
+
+namespace CSharpContactListCrud.controllers
+{
+    internal class ContactListController
+    {
+        static List<Contact> contacts = new List<Contact>();
+
+        public void viewContacts()
+        {
+            if (contacts.Count < 1)
+            {
+                Console.WriteLine("Telefonda heç bir kontakt yoxdur");
+            }
+            else
+            {
+                foreach (Contact contact in contacts)
+                {
+                    Console.Write($"Kontakt id: {contact.Id} , ");
+                    Console.Write($"Kontakt adı: {contact.FullName} , ");
+                    Console.WriteLine($"Kontakt nömrəsi: {contact.Number} , ");
+                }
+            }
+        }
+
+        public void addContact(string FullName, string Number)
+        {
+            int Id;
+            if (contacts.Count < 1)
+            {
+                Id = 0;
+            }
+            else
+            {
+                Contact LastElementOfList = contacts[contacts.Count - 1];
+                Id = LastElementOfList.Id + 1;
+            }
+            Contact contact = new Contact
+            {
+                Id = Id,
+                FullName = FullName,
+                Number = Number
+            };
+            contacts.Add(contact);
+        }
+
+        public void removeContact(int id)
+        {
+            var itemToRemove = contacts.SingleOrDefault(r => r.Id == id);
+            if (itemToRemove != null)
+            {
+                contacts.Remove(itemToRemove);
+                Console.WriteLine($"{itemToRemove.FullName} adlı şəxs kontakt siyahınızdan silindi.");
+            }
+        }
+
+        public void SearchContact(string query, int searchType)
+        {
+            IEnumerable<Contact> results = null;
+
+            switch (searchType)
+            {
+                case 1:
+                    results = contacts.Where(c => c.FullName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0);
+                    break;
+                case 2:
+                    results = contacts.Where(c => c.Number.Contains(query));
+                    break;
+                case 3:
+                    if (int.TryParse(query, out int id))
+                    {
+                        results = contacts.Where(c => c.Id == id);
+                    }
+                    break;
+            }
+
+            if (results != null && results.Any())
+            {
+                foreach (var contact in results)
+                {
+                    Console.Write($"Kontakt id: {contact.Id} , ");
+                    Console.Write($"Kontakt adı: {contact.FullName} , ");
+                    Console.WriteLine($"Kontakt nömrəsi: {contact.Number} , ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Heç bir uyğun kontakt tapılmadı.");
+            }
+        }
+
+        public void updateContactName(int id, string newFullName)
+        {
+            var contactToUpdate = contacts.SingleOrDefault(r => r.Id == id);
+            if (contactToUpdate != null)
+            {
+                contactToUpdate.FullName = newFullName;
+                Console.WriteLine($"Kontakt {id} yeniləndi.");
+            }
+            else
+            {
+                Console.WriteLine($"ID {id} olan kontakt tapılmadı.");
+            }
+        }
+
+        public void updateContactNumber(int id, string newNumber)
+        {
+            var contactToUpdate = contacts.SingleOrDefault(r => r.Id == id);
+            if (contactToUpdate != null)
+            {
+                contactToUpdate.Number = newNumber;
+                Console.WriteLine($"Kontakt {id} yeniləndi.");
+            }
+            else
+            {
+                Console.WriteLine($"ID {id} olan kontakt tapılmadı.");
+            }
+        }
+    }
+}
